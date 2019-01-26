b41b12ca196574ee23ffbddd6151a4841e1f7bf8
Maher Khan <khanm167@mcmaster.ca>
2019-01-26 17:01:46 -0500

    set up db

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express ();
+var mongoose = require("mongoose")
+mongoose.connect("mongodb://deltahacks:deltahacks1@ds213755.mlab.com:13755/deltahacks",  { useNewUrlParser: true });
 
 app.get("/", function(req, res){
     res.send("hello");
